13704a3882d59cb51edfdc8297418b96e82303d8
SainsateSonik <sainsatesonik@gmail.com>
2019-01-06 18:33:20 +0530

    Updates

---
+++ precache-manifest.95674280b0573e699c3770460369469a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9579c88140ea01fd83d85ff2d1418b28",
+    "url": "/pricingTool/static/media/estimation.9579c881.png"
+  },
+  {
+    "revision": "ca1f28f733e39dfa8ba8ae0dfd363c8c",
+    "url": "/pricingTool/static/media/analysis.ca1f28f7.png"
+  },
+  {
+    "revision": "885506c7a784494732aa",
+    "url": "/pricingTool/static/js/runtime~main.885506c7.js"
+  },
+  {
+    "revision": "117ff1f9a7aa610a777f",
+    "url": "/pricingTool/static/js/main.117ff1f9.chunk.js"
+  },
+  {
+    "revision": "f14bac9fbc815f697494",
+    "url": "/pricingTool/static/js/1.f14bac9f.chunk.js"
+  },
+  {
+    "revision": "117ff1f9a7aa610a777f",
+    "url": "/pricingTool/static/css/main.1144780e.chunk.css"
+  },
+  {
+    "revision": "7985d02f5a5886d73505f374bc25a4fb",
+    "url": "/pricingTool/index.html"
+  }
+];
